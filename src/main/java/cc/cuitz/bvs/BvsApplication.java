8eb0e37f60afa59411f00b0d99567c2f319c6d4f
cuitongzhe <cuitongzhe@vackbot.com>
2023-03-17 18:36:03 +0800

    first commit

---
+++ src/main/java/cc/cuitz/bvs/BvsApplication.java
@@ -0,0 +1,13 @@
+package cc.cuitz.bvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BvsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/cuitz/bvs/BvsApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.cuitz.bvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
