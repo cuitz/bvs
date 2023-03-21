54c28dd229be5fe7e80c9c85809fc5c83d44fe5e
cuitongzhe <cuitongzhe@vackbot.com>
2023-03-21 14:10:41 +0800

    fix

--- src/main/java/cc/cuitz/bvs/vo/common/PageParam.java
+++ src/main/java/cc/cuitz/bvs/vo/common/PageParam.java
@@ -1,10 +1,13 @@
 package cc.cuitz.bvs.vo.common;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.Data;
+import org.springframework.util.StringUtils;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * 前端传来的分页参数
@@ -22,5 +25,27 @@ public class PageParam {
     @Min(1)
     private Integer pageSize;
 
-    private String orderBy;
+    private List<String> sort;
+
+    public String getOrderBy() {
+        if (sort == null || sort.isEmpty()) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : sort) {
+            if (StringUtils.hasText(s)) {
+                List<String> strings = StrUtil.split(s, " ");
+                if (strings.size() > 2) {
+                    return "";
+                }
+                if (strings.size() == 2) {
+                    sb.append(StrUtil.toUnderlineCase(strings.get(0))).append(" ").append(strings.get(1));
+                } else {
+                    sb.append(StrUtil.toUnderlineCase(strings.get(0)));
+                }
+                sb.append(",");
+            }
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
 }
